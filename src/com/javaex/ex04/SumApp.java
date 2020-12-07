71bd2976f134aedbb2e224d7e65f6593c8406da4
tjdndrl0922@naver.com <1@DESKTOP-BDRSOA3>
2020-12-07 18:34:56 +0900

    Practice08

---
+++ src/com/javaex/ex04/SumApp.java
@@ -0,0 +1,26 @@
+package com.javaex.ex04;
+
+import java.util.Scanner;
+
+public class SumApp {
+
+	public static void main(String[] args) {
+		
+		int sum = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("숫자를 공백으로 구분해서 입력하세요");
+		String numLine = sc.nextLine();
+
+		//코드를 작성하세요
+		for(int i=0; i<numLine.length(); i++) {
+			
+			sum += (numLine.charAt(i) - '0');
+		}
+		
+		
+		System.out.println("합:" + sum);
+		
+		sc.close();
+	}
+
+}
